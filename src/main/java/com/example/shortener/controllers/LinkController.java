e752ab99f22dafdcf27d9cb29d9b9bfed37558c3
Olga Iamshchikova <oljabotova@gmail.com>
2024-04-02 21:24:39 +0200

    added validation for submit form

--- src/main/java/com/example/shortener/controllers/LinkController.java
+++ src/main/java/com/example/shortener/controllers/LinkController.java
@@ -1,9 +1,12 @@
 package com.example.shortener.controllers;
 
+import com.example.shortener.entity.ui.SubmitForm;
 import com.example.shortener.service.LinkService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,14 +15,29 @@ public class LinkController {
     @Autowired
     private LinkService service;
 
+//    @GetMapping(value = "/")
+//    public String home(Model model) {
+//        return "home";
+//    }
+
     @GetMapping(value = "/")
     public String home(Model model) {
+        model.addAttribute("submitForm", new SubmitForm());
         return "home";
     }
 
+//    @PostMapping("/")
+//    public String shortenUrl(@RequestParam("longUrl") String longUrl, Model model) {
+//        model.addAttribute("shortUrl", service.shortenUrl(longUrl));
+//        return "short_url";
+//    }
+
     @PostMapping("/")
-    public String shortenUrl(@RequestParam("longUrl") String longUrl, Model model) {
-        model.addAttribute("shortUrl", service.shortenUrl(longUrl));
+    public String shortenUrl(@Valid @ModelAttribute("submitForm") SubmitForm submitForm, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            return "home";
+        }
+        model.addAttribute("shortUrl", service.shortenUrl(submitForm.getUrl()));
         return "short_url";
     }
 
