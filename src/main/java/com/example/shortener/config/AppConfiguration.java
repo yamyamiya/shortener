28d87a44025d6fa03e0d8d59a9a895dc7d51f4b5
Olga Iamshchikova <oljabotova@gmail.com>
2024-04-08 22:11:22 +0200

    added i18n (en, de)

---
+++ src/main/java/com/example/shortener/config/AppConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.shortener.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        localeResolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
+        return localeResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("localeData");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:lang/messages");
+        return messageSource;
+    }
+
+}
