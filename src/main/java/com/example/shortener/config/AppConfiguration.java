4acc0ec9e003007d71f1c0d4eb1f53292579311e
Olga Iamshchikova <oljabotova@gmail.com>
2024-04-10 21:24:01 +0200

    added i18n (fr, it, ru), UTF-8 for IntelliJ IDEA to support russian

--- src/main/java/com/example/shortener/config/AppConfiguration.java
+++ src/main/java/com/example/shortener/config/AppConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -14,6 +13,14 @@ import java.util.TimeZone;
 
 @Configuration
 public class AppConfiguration {
+
+    /**
+     * The LocaleResolver interface deals with locale resolution required when localizing web applications to specific locales.
+     * CookieLocaleResolver — resolves the locale and stores it in a cookie stored on the user’s machine.
+     * Now, as long as browser cookies aren’t cleared by the user, once resolved the resolved locale data
+     * will last even between sessions.
+     */
+
     @Bean
     public LocaleResolver localeResolver() {
         CookieLocaleResolver localeResolver = new CookieLocaleResolver();
@@ -21,18 +28,31 @@ public class AppConfiguration {
         localeResolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
         return localeResolver;
     }
+
+    /**
+     * Interceptor bean will intercept each request that the application receives,
+     * and check for a localeData parameter on the HTTP request.
+     * If found, the interceptor uses the localeResolver to register
+     * the locale it found as the current user’s locale.
+     */
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
+    public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("localeData");
         return localeChangeInterceptor;
     }
 
+    /**
+     * The purpose of this bean is to provide message resources for internationalization (i18n)
+     * in a Spring application.
+     * MessageSource Class manages and access message resources for internationalization purposes.
+     * It's an essential part of building applications that support multiple languages and locales.
+     */
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:lang/messages");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-
 }
