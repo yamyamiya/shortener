c9c7b76f0bc337d415d133daac9d8835d098e80c
OLGA IAMSHCHIKOVA <54511031+yamyamiya@users.noreply.github.com>
2024-05-09 19:45:45 +0200

    added tests for new methods (#12)

--- src/test/java/com/example/shortener/controllers/LinkControllerTest.java
+++ src/test/java/com/example/shortener/controllers/LinkControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -60,6 +61,15 @@ class LinkControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"));
     }
+
+@Test
+public void shouldReturnCorrectLink() throws Exception {
+        String longUrl = "https://example.com";
+        when(service.returnOriginalUrl("shortUrl")).thenReturn(longUrl);
+        mockMvc.perform(get("/original/shortUrl"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(longUrl));
+}
 
     @Test
     public void shouldCorrectlyRedirectToOriginalUrl() throws Exception {
