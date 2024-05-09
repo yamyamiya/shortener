43778d05bda84e99a3ca666fd7ee0b5101a4a57a
OLGA IAMSHCHIKOVA <54511031+yamyamiya@users.noreply.github.com>
2024-05-09 18:50:59 +0200

    added swagger doc and generated openApi JSON (#11)

--- src/main/java/com/example/shortener/controllers/LinkController.java
+++ src/main/java/com/example/shortener/controllers/LinkController.java
@@ -2,8 +2,10 @@ package com.example.shortener.controllers;
 
 import com.example.shortener.entity.ui.SubmitForm;
 import com.example.shortener.service.LinkService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,13 +17,16 @@ public class LinkController {
     @Autowired
     private LinkService service;
 
+
     @GetMapping(value = "/")
+    @Operation(summary = "Get the home page.")
     public String home(Model model) {
         model.addAttribute("submitForm", new SubmitForm());
         return "home";
     }
 
     @PostMapping("/")
+    @Operation(summary = "Generate short URL for provided long URL.")
     public String shortenUrl(@Valid @ModelAttribute("submitForm") SubmitForm submitForm, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()){
             return "home";
@@ -30,8 +35,16 @@ public class LinkController {
         return "short_url";
     }
 
+
     @GetMapping("/{shortUrl}")
+    @Operation(summary = "Redirect to the corresponding long URL.")
     public ModelAndView redirectToOriginalUrl(@PathVariable String shortUrl) {
         return new ModelAndView(service.redirectToOriginalUrl(shortUrl));
+    }
+
+    @GetMapping("/original/{shortUrl}")
+    @Operation(summary = "Return the corresponding long URL.")
+    public ResponseEntity<String> returnOriginalUrl(@PathVariable String shortUrl) {
+        return ResponseEntity.ok(service.returnOriginalUrl(shortUrl));
     }
 }
